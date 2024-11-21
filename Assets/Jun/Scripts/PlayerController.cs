dcf611da31ccfd1441d96bd5abb32489b35034ba
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-11-21 15:33:06 +0900

    [Create] 임시 블럭 프리팹 생성
    임시 블럭 프리팹 생성
    
    블럭 스크립트의 이벤트 구독, 구독해제 및 그에 따른 현재 블럭 관리 프로토타입  구현

--- Assets/Jun/Scripts/PlayerController.cs
+++ Assets/Jun/Scripts/PlayerController.cs
@@ -6,15 +6,35 @@ using Photon.Pun.Demo.Procedural;
 
 public class PlayerController : MonoBehaviourPun
 {
-    private Transform spawnPoint;
-    private GameObject[] blockPrefabs;
-    private Block currentBlock;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject[] blockPrefabs;
+    [SerializeField] private Block currentBlock;
+
 
     private void Start()
     {
+        PhotonNetwork.ConnectUsingSettings();
         if (photonView.IsMine)
         {
+
+            if (spawnPoint == null || blockPrefabs == null || blockPrefabs.Length == 0)
+            {
+                Debug.LogError("스폰지점, 블럭프리팹이 잘못 설정 되었습니다.");
+                return;
+            }
+
+            //Start가 아닌 방에 참가 했을 때 스폰하는 것으로 수정 예정
             SpawnBlock();
+
+            //Block.OnBlockEntered += BlockEntered;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (photonView.IsMine)
+        {
+            //Block.OnBlockEntered -= BlockEntered;
         }
     }
 
@@ -51,11 +71,25 @@ public class PlayerController : MonoBehaviourPun
         }
             
     }
+    private void BlockEntered()
+    {
+        // 기존 블럭의 제어 해제
+        if (currentBlock != null)
+        {
+            currentBlock = null;
+        }
+
+        // 새로운 블럭 생성
+        SpawnBlock();
+    }
 
     public void SpawnBlock()
     {
-        if (!PhotonNetwork.IsConnected)
+        if (!PhotonNetwork.IsConnected) 
+        {
+            Debug.LogError("Photon Network에 연결되어 있지 않습니다. 블럭을 생성할 수 없습니다.");
             return;
+        }
 
         int randomIndex = Random.Range(0, blockPrefabs.Length);
         GameObject newBlock = PhotonNetwork.Instantiate(blockPrefabs[randomIndex].name, spawnPoint.position, Quaternion.identity);
