fda1a8078e00bfac89aa69cb6d32aeff83470bb9
GuGuJelly <jellypuding@naver.com>
2024-11-22 16:37:19 +0900

    [Feat] 플레이어의 룸패널 입장

---
+++ Assets/HwangGyuHo/HGH_Scripts/CustomPropert.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
+
+public static class CustomPropert
+{
+    public const string READY = "Ready";
+
+    private static PhotonHashtable customProperty = new PhotonHashtable();
+    // 확장메소드
+    public static void SetReady(this Player player, bool ready)
+    {
+        customProperty.Clear();
+        customProperty[READY] = ready;
+        player.SetCustomProperties(customProperty);
+    }
+
+    public static bool GetReady(this Player player)
+    {
+        PhotonHashtable customProperty = player.CustomProperties;
+        if (customProperty.ContainsKey(READY))
+        {
+            return (bool)customProperty[READY];
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
--- Assets/HwangGyuHo/HGH_Scripts/RoomPanel.cs
+++ Assets/HwangGyuHo/HGH_Scripts/RoomPanel.cs
@@ -1,18 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 
 public class RoomPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] PlayerEntry[] playerEntries;
+    [SerializeField] Button startButton;
+
+
+    private void OnEnable()
     {
-        
+        // PlayerNumbering 에 플레이어 추가
+        PlayerNumbering.OnPlayerNumberingChanged += UpdatePlayer;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        // PlayerNumbering에 플레이어 빼기
+        PlayerNumbering.OnPlayerNumberingChanged -= UpdatePlayer;
+    }
+
+    public void UpdatePlayer()
+    {
+        // 
+        foreach (PlayerEntry entry in playerEntries)
+        {
+            entry.SetEmpty();
+        }
+
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.GetPlayerNumber() == -1)
+                return;
+
+            int number = player.GetPlayerNumber();
+            playerEntries[number].SetPlayer(player);
+        }
+    }
+
+    public void PlayerEnterRoom(Player newPlayer)
+    {
+        Debug.Log($"{newPlayer.NickName} 입장!");
+        UpdatePlayer();
+    }
+
+    public void PlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log($"{otherPlayer.NickName} 퇴장!");
+        UpdatePlayer();
+    }
+
+    public void PlayerPropertiesUpdate(Player targetPlayer)
+    {
+        // TODO : 플레이어 속성이 바뀌면 그것을 업데이트
+    }
+
+    public void MasterClientSwitched(Player newMasterClient)
+    {
+        Debug.Log($"{newMasterClient.NickName}이 방장이 됐습니다.");
+    }
+
+    public void StartGame()
+    {
+        // TODO : 플레이어들 READY가 모두 되면 게임시작 버튼으로 게임시작
+    }
+
+    public void LeaveRoom()
+    {
+        Debug.Log("방을 떠났습니다");
+    }
+
+    public void AllPlayerReadyCheck()
+    {
+        // TODO : 모든 플레이어의 레디 체크
     }
 }
