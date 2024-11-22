b85d16a0e88801048f5dbcf2b35fe11b618c5abc
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-11-22 15:44:38 +0900

    [Feat] 블럭 개수, 최대 높이를 계산하는 매니저 프로토타입 구현
    블럭 개수 및 최대 높이를 계산하는 매니저 프로토타입 구현

--- Assets/Jun/Scripts/PlayerController.cs
+++ Assets/Jun/Scripts/PlayerController.cs
@@ -12,13 +12,16 @@ public class PlayerController : MonoBehaviourPun
     [Header("Block")]
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private GameObject[] blockPrefabs;
-    [SerializeField] private Block currentBlock;
+    [SerializeField] private Blocks currentBlock;
 
 
     [Header("PlayerStat")]
     [SerializeField] private int curHp;
     [SerializeField] private int maxHp;
-    public event Action OnChangeHp;
+    public event Action<int> OnChangeHp;
+
+    [Header("PlayerCheck")]
+    public bool IsGoal = false;
 
     private void Start()
     {
@@ -49,7 +52,7 @@ public class PlayerController : MonoBehaviourPun
 
     private void Update()
     {
-        if (photonView.IsMine && currentBlock != null)
+        if (photonView.IsMine && currentBlock != null )
         {
             PlayerInput();
         }
@@ -57,28 +60,29 @@ public class PlayerController : MonoBehaviourPun
 
     private void PlayerInput()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            //currentBlock.Move();
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            //currentBlock.Move();
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
-        {
-            //currentBlock.Rotate(90);
-        }
-
+        if (!IsGoal)
+            return;
 
         if (Input.GetKey(KeyCode.DownArrow))
+            currentBlock.Down();
+
+        if (Input.GetKey(KeyCode.U))
         {
-            //currentBlock.Down();
+            currentBlock.Push(Vector2.left);
         }
-            
+        else if (Input.GetKey(KeyCode.I))
+        {
+            currentBlock.Push(Vector2.right);
+        }
+        else
+        {
+            currentBlock.Move(Vector2.right * Input.GetAxisRaw("Horizontal"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentBlock.Rotate();
+        }
     }
     private void BlockEntered()
     {
@@ -103,13 +107,15 @@ public class PlayerController : MonoBehaviourPun
         int randomIndex = Random.Range(0, blockPrefabs.Length);
         //SpawnPoint는 플레이어 위치 or 블럭의 쌓인 y값 최대치 or 타워의 높이 상대치를 통해 정해질 예정
         GameObject newBlock = PhotonNetwork.Instantiate(blockPrefabs[randomIndex].name, spawnPoint.position, Quaternion.identity);
-        currentBlock = newBlock.GetComponent<Block>();
+        currentBlock = newBlock.GetComponent<Blocks>();
     }
 
     public void TakeDamage(int damage)
     {
         curHp -= damage;
         Debug.Log($"현재 체력 : {curHp}");
+
+        OnChangeHp?.Invoke(curHp);
 
         // 플레이어의 체력이 0 이하가 되면 그 이후의 상황을 처리
         if (curHp <= 0)
@@ -122,8 +128,12 @@ public class PlayerController : MonoBehaviourPun
     {
         //플레이어 죽음 애니메이션 추가
         //한명의 플레이어가 생존할 때 까지 대기
-        OnChangeHp?.Invoke();
     }
 
-
+    public void ReachGoal()
+    {
+        IsGoal = true;
+        //골인 했을 때 추가적인 구현
+        //ex) 원작 게임처럼 큰 나무집이 떨어져 1등이 엔딩을 장식할 수 있도록
+    }
 }
