c8b752c741f1022f3c709d4c06bd120ea2933786
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-11-22 14:12:53 +0900

    [FIx] 코루틴 관련 특이사항 수정
    - 코루틴 관련 문제 해결
    - 블럭 관련 참조 설정
    - 일부 변수 용어 수정

--- Assets/KwakSeongDae/Scripts/GameStates/PuzzleModeState.cs
+++ Assets/KwakSeongDae/Scripts/GameStates/PuzzleModeState.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class PuzzleModeState : GameState
 {
     [Header("퍼즐 모드 설정")]
-    [SerializeField] private float finishAcceptTime;
+    [SerializeField] private float finishTimer;
     [SerializeField] private BoxCollider2D boxDetector;
     private Dictionary<int, Coroutine> finishRoutineDic;
     private Dictionary<int, bool> isBlockCheckDic;
@@ -27,12 +27,9 @@ public class PuzzleModeState : GameState
             finishRoutineDic.Add(playerID, null);
             isBlockCheckDic.Add(playerID, false);
         }
-        // 테스트 용으로 해당 코드 추가
-        finishRoutineDic.Add(0, null);
-        isBlockCheckDic.Add(0, false);
 
         // Timer 초기 세팅
-        timer = new WaitForSeconds(finishAcceptTime);
+        timer = new WaitForSeconds(finishTimer);
 
         // 충돌 감지 루틴 실행
         mainCollisionRoutine = StartCoroutine(CollisionCheckRoutine());
@@ -40,7 +37,6 @@ public class PuzzleModeState : GameState
 
     public override void Exit()
     {
-        base.Exit();
         StopCoroutine(mainCollisionRoutine);
         // finishRoutine이 실행되고 있는 경우에는 해당 코루틴은 중지
         foreach (int i in finishRoutineDic.Keys)
@@ -52,22 +48,28 @@ public class PuzzleModeState : GameState
         }
         finishRoutineDic.Clear();
         isBlockCheckDic.Clear();
+
+        // Exit호출은 Enter의 역순
+        base.Exit();
     }
     private IEnumerator CollisionCheckRoutine()
     {
         var detectorPos = (Vector2)boxDetector.transform.position + boxDetector.offset;
         var detectorScale = Vector2.Scale(boxDetector.transform.localScale, boxDetector.size);
         var delay = new WaitForSeconds(0.1f);
+        // 코루틴 실행 즉시 실행 하지 말기 => 로그가 더러워짐
+        yield return null;
+
         while (true)
         {
-            // 현재 블럭 충돌 Check를 False로 초기화
+            // 1. 현재 블럭 충돌 Check를 False로 초기화
             var isBlockCheckKeys = isBlockCheckDic.Keys.ToArray();
             foreach (var key in isBlockCheckKeys)
             {
                 isBlockCheckDic[key] = false;
             }
 
-            // Physics2D로 충돌체 검사
+            // 2. Physics2D로 충돌체 검사
             // isEntered가 된 블럭만 감지해서 현재 FInish 지점 상태 업데이트
             Collider2D[] cols = Physics2D.OverlapBoxAll(detectorPos, detectorScale, 0);
             print("충돌 감지 중");
@@ -78,18 +80,19 @@ public class PuzzleModeState : GameState
                 // 블럭이 존재하는 경우, 해당 소유자의 블럭이 있음을 체크
                 // 충돌된 블럭이 있을때, 플레이어의 코루틴의 유무 판단 후, 코루틴 실행
                 if (collision.CompareTag("Player")
-                    //&& collision.GetComponent<Block>().isEntered == true
+                    && collision.GetComponent<Blocks>().IsEntered == false
                     && collision.TryGetComponent<PhotonView>(out var block))
                 {
+                    // 테스트 용
                     //int playerID = block.Owner.ActorNumber;
                     int playerID = 0;
-                    print("충돌 됨");
+
                     if (isBlockCheckDic.ContainsKey(playerID))
                         isBlockCheckDic [playerID] = true;
                 }
             }
 
-            // 현재 충돌된 블럭이 있는 플레이어들만 FInishRoutine 수행
+            // 3. 현재 충돌된 블럭이 있는 플레이어들만 FInishRoutine 수행
             // 충돌된 블럭이 없는 플레이어들은 기존 수행되던 루틴을 해제
             var finishRoutineKeys = finishRoutineDic.Keys.ToArray();
             foreach (var playerID in finishRoutineKeys)
@@ -99,30 +102,23 @@ public class PuzzleModeState : GameState
                 {
                     if (isBlockCheckDic[playerID] == true)
                     {
-                        finishRoutineDic[playerID] = StartCoroutine(FinishRoutine(playerID));
                         print($"{playerID} 블럭 감지");
+                        if (finishRoutineDic[playerID] == null)
+                            finishRoutineDic[playerID] = StartCoroutine(FinishRoutine(playerID));
                     }
                     else
                     {
+                        print($"{playerID} 블럭 없음");
                         if (finishRoutineDic[playerID] != null)
                         {
-                            // 코루틴이 정상적으로 종료되지 않는 문제 발생
-                            print(finishRoutineDic[playerID]);
                             StopCoroutine(finishRoutineDic[playerID]);
-                            print(finishRoutineDic[playerID]);
                             finishRoutineDic[playerID] = null;
-                            print($"{playerID} 블럭 없음");
-                        }
-                        else
-                        {
-                            print($"{playerID} 코루틴 없음");
                         }
                     }
                 }
                 else
                 {
-                    print(playerID);
-                    print("기존에 참가하지 않은 플레이어가 접근");
+                    print($"{playerID}의 비정상적인 접근");
                 }
             }
             yield return delay;
