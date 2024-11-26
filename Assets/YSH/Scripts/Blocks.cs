d73b480fc205325f1426770008a7e94e4b4ad5db
dhghgh189 <dhghgh189@gmail.com>
2024-11-26 15:04:20 +0900

    [Feat] 블럭 제어권 상실 시 발생할 이벤트 추가
    - 블럭 제어권 상실 시 이벤트를 발생시켜 블럭 스폰에 활용할 수 있도록 함

--- Assets/YSH/Scripts/Blocks.cs
+++ Assets/YSH/Scripts/Blocks.cs
@@ -40,7 +40,8 @@ public class Blocks : MonoBehaviourPun
     public UnityAction OnBlockFallen;       // 블럭이 맵밖으로 떨어질 때 Invoke (체력감소, 사망 처리에 활용)
     public UnityAction OnBlockEntered;      // 블럭이 안착했을 때 Invoke (블럭 카운팅에 활용)
     public UnityAction OnBlockExited;       // 블럭이 안착했다가 벗어날 때 (블럭 카운팅에 활용)
-
+    public UnityAction OnDisableControl;    // 블럭의 제어권이 상실될 때 (블럭 스폰에 활용)
+  
     public bool IsEntered { get { return isEntered; } }     // 외부에서 안착여부를 확인하기 위한 프로퍼티
 
     private void Awake()
@@ -277,6 +278,9 @@ public class Blocks : MonoBehaviourPun
                 // 제어권을 해제
                 isControllable = false;
 
+                // 이벤트 발생
+                OnDisableControl?.Invoke();
+
                 // spotlight 비활성화
                 spotlightObject.SetActive(false);
 
@@ -330,6 +334,9 @@ public class Blocks : MonoBehaviourPun
 
             // 충돌 시 flag 변경 
             isControllable = false;
+
+            // 이벤트 발생
+            OnDisableControl?.Invoke();
         }
 
         // 충돌체 카운트 증가
