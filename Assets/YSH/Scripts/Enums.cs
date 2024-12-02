2190ffe5290165cc78db5f1e233360414f80dece
dhghgh189 <dhghgh189@gmail.com>
2024-12-02 12:57:36 +0900

    [Fix] 불필요한 Enum 제거

--- Assets/YSH/Scripts/Enums.cs
+++ Assets/YSH/Scripts/Enums.cs
@@ -4,32 +4,6 @@ using UnityEngine;
 
 public static class Enums
 {
-    public enum SkillType
-    {
-        Basic,      // 기본
-        Fire,       // 불
-        Water,      // 물
-        Thunder,    // 번개
-        Wind        // 바람
-    }
-
-    public enum Grade
-    {
-        Basic,      // 기본
-        Low,        // 하급
-        Mid,        // 중급
-        High        // 상급
-    }
-
-    public enum PlayerSkillSlot
-    {
-        Slot1,
-        Slot2,
-        Slot3,
-        Slot4,
-        Length,
-    }
-
     public enum ESoundType
     {
         BGM,
